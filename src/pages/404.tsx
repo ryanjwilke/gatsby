11e7e68b49734c7729ed06ce081c64afee274c81
Ryan Wilke <ryanwilke@gmail.com>
2020-08-19 19:36:55 +0200

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,12 +5,10 @@ import { Helmet } from 'react-helmet'
 export default () => {
   return (
     <Layout>
-      <main>
-        <Helmet>
-          <title>404 - Page Not Found</title>
-        </Helmet>
-        <h1>404 - Page Note Found</h1>
-      </main>
+      <Helmet>
+        <title>404 - Page Not Found</title>
+      </Helmet>
+      <h1>404 - Page Note Found</h1>
     </Layout>
   );
 }
