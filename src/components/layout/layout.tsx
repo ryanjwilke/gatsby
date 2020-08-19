32f1d4d034be1b8dc9b2f5ce265a281cbd05426f
Ryan Wilke <ryanwilke@gmail.com>
2020-08-19 22:00:50 +0200

    Update layout.tsx

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { Helmet } from 'react-helmet'
 import './layout.sass'
 
 export default function Layout({ children }) {
   return (
     <div className="layout">
+      <Helmet title="Design System">
+        <html lang="en" />
+      </Helmet>
       <nav>
         <h6>Design System</h6>
         <menu>
